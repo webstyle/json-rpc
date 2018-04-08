1241aa022e5bcfff24d07f60fd49046dd3c5f3eb
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2018-04-08 21:46:07 +0500

    added http client

--- rpc_client.js
+++ rpc_client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const http = require('http');
 
 class tcp_client {
     constructor({ host = 'localhost', port }) {
@@ -6,7 +7,7 @@ class tcp_client {
             if (err) throw err;
         });
     }
-    request(method_name, params) {
+    async request(method_name, params) {
         return new Promise((resolve, reject) => {
             this.clinet.write(JSON.stringify({ method: method_name, params }), (err) => {
                 if (err) reject(err);
@@ -16,9 +17,34 @@ class tcp_client {
                 if (method === method_name) return resolve({ result, error });
             });
         })
+    }
+}
 
+class http_client {
+    constructor({ host = `http://localhost`, port }) {
+        this.host = host;
+        if (!port) throw new Error(`port is required`);
+        this.port = port;
+    }
+    async request(method, params) {
+        const options = {
+            hostname: this.host,
+            port: this.port,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        return new Promise((resolve, reject) => {
+            const req = http.request({ options }, async res => {
+                res.setEncoding(`utf-8`);
+                res.on(`data`, chunk => resolve(JSON.parse(chunk)));
+            });
+            req.write({ method, params });
+        });
     }
 }
 
 
-module.exports = tcp_client;
+module.exports.tcp_client = tcp_client;
+module.exports.http_client = http_client;
